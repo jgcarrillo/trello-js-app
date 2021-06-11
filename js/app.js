6c3cfce8dbca3af853294ec955fad345214ae4d7
Jorge G <jorgegomezweb@gmail.com>
2021-06-11 18:41:33 +0200

    FEAT: Adding grid layout

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+	// VARIABLES
+	const btnTodo = document.querySelector('.add-todo');
+	const btnDoing = document.querySelector('.add-doing');
+	const btnDone = document.querySelector('.add-done');
+});
