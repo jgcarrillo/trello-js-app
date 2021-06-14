af4dd4b76005fe0fb2a978c98dd6fc275586c5b8
Jorge G <jorgegomezweb@gmail.com>
2021-06-14 11:44:13 +0200

    FIX: Adding js containers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-	// VARIABLES
+	// GLOBAL VARIABLES
 	const btnTodo = document.querySelector('.add-todo');
 	const btnDoing = document.querySelector('.add-doing');
 	const btnDone = document.querySelector('.add-done');
+	const containerToDo = document.querySelector('.container__todo');
+	const containerDoing = document.querySelector('.container__doing');
+	const containerDone = document.querySelector('.container__done');
 });
